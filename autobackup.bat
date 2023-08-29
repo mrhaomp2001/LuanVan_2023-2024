bcceb0b9f565bf3b402e8d5feddc2ad5fe8ecdd9
hao.sensornotes <hao.sensornotes@gmail.com>
2023-08-29 09:13:44 +0700

    auto backup 2023-08-29_09-13-44

---
+++ autobackup.bat
@@ -0,0 +1,12 @@
+@echo on
+title Git Auto Backup
+color 1b
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+git add .
+git commit -m "auto backup "%fullstamp%""
