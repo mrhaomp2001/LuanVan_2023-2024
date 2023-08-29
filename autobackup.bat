1c9102a52c16ca144a70b85ef0e17679b76722a5
hao.sensornotes <hao.sensornotes@gmail.com>
2023-08-29 09:14:51 +0700

    auto backup 2023-08-29:09-14-51

--- autobackup.bat
+++ autobackup.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 title Git Auto Backup
 color 1b
 
@@ -7,7 +7,7 @@ set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 
 set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
-set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%:%HH%-%Min%-%Sec%"
 git add .
 git commit -m "auto backup "%fullstamp%""
 set /p DUMMY=Hit ENTER to continue...
