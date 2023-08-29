25baa28a30d013efb414e007a27607d77681b159
hao.sensornotes <hao.sensornotes@gmail.com>
2023-08-29 09:16:59 +0700

    auto backup 2023-08-29:09-16-59

--- autobackup.bat
+++ autobackup.bat
@@ -8,5 +8,5 @@ set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "fullstamp=%YYYY%-%MM%-%DD%:%HH%-%Min%-%Sec%"
 git add .
-git commit -m "auto backup "%fullstamp%""
-set /p DUMMY=Hit ENTER to continue...
+git commit -m "auto backup %fullstamp%"
+git push origin master
