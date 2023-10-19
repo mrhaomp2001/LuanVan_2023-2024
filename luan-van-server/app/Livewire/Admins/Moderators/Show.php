c94320ece4aad6d362fb537debe376489205ac13
Huynh Nhut Hao <hao.sensornotes@gmail.com>
2023-10-19 15:23:03 +0700

    auto backup 2023/10/19 15:23:02

---
+++ luan-van-server/app/Livewire/Admins/Moderators/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admins\Moderators;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.admins.moderators.show');
+    }
+}
